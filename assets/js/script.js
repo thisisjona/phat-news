68320beb2b5207f3231344cd450afdb9c3a521b3
crocket77 <ethanpfeiffer345@outlook.com>
2022-02-03 19:50:20 -0600

    articles from sports API is now in sportsArr

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,21 @@
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#futbol_team");//futbol team 
+let sportsArr=[];
 
 //get form data
 var formSubmitHandler = function(event) {
   // prevent page from refreshing
   event.preventDefault();
-
   // get value from input element
   var country = nameInputEl.value.trim();
-     
   if (country) {
-    getSportNews(country);
-
+    getUserRepos(country);
     // clear old content
-    userFormEl.textContent = "";
     nameInputEl.value = "";
   } else {
     alert("Please enter a country");
   }
-};
-
-// https://newsapi.org/v2/everything?q=tesla&from=2022-01-04&sortBy=publishedAt&apiKey=ade84378e67548e5b4ed1e45d4c09606
-// https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=ade84378e67548e5b4ed1e45d4c09606
-let sportsArr=[];
+}
 
 //Sports new API
 var getSportNews = function() {
@@ -31,64 +24,22 @@ var getSportNews = function() {
     // make a get request to url
     fetch(apiUrl)
       .then(function(response) {
-        response.json().then(function(data) {
-        console.log(data);
-        })
+        //request was successful
+        if(response.ok){
+          response.json().then(function(data) {
+          sportsArr=data.articles;
+          console.log("sports array ", sportsArr)     
+         });
+        }else {
+          alert("Error: " + response.statusText);
+        }
       })
+      .catch(function(error) {
+        alert("Unable to connect to server");
+      });
   };
   getSportNews();
 
-  var getOtherNews = function() {
-    // format the github api url
-    var apiUrl = "https://api.sportsdata.io/v3/nfl/scores/json/News?key=95bd4e03de4e4fe0916f0c77516e239c";
-    // make a get request to url
-    fetch(apiUrl)
-      .then(function(response) {
-        response.json().then(function(data) {
-        console.log(data);
-        })
-      })
-  };
-  getOtherNews();
-
-   //   console.log(response)
-      //   // request was successful
-      //   if (response.ok) {
-      //     console.log(response);
-      //     response.json().then(function(data) {
-      //       console.log(data);
-      //       displayRepos(data, country);
-      //     });
-      //   } else {
-      //     alert("Error: " + response.statusText);
-      //   }
-      // })
-      // .catch(function(error) {
-      //   alert("Unable to connect to GitHub");
-      // });
-  // //2nd API fetch
-  // var getSportsWeather = function(user) {
-  //   // format the github api url
-  //   var apiUrl = "";
-  
-  //   // make a get request to url
-  //   fetch(apiUrl)
-  //     .then(function(response) {
-  //       // request was successful
-  //       if (response.ok) {
-  //         console.log(response);
-  //         response.json().then(function(data) {
-  //           console.log(data);
-  //           displayRepos(data, user);
-  //         });
-  //       } else {
-  //         alert("Error: " + response.statusText);
-  //       }
-  //     })
-  //     .catch(function(error) {
-  //       alert("Unable to connect to GitHub");
-  //     });
-  // };
 
 // get news
   var getFeaturedRepos = function(language) {
@@ -110,9 +61,8 @@ var getSportNews = function() {
       repoContainerEl.textContent = "No repositories found.";
       return;
     }
-  
     repoSearchTerm.textContent = searchTerm;
-  
+    
     // loop over repos
     for (var i = 0; i < repos.length; i++) {
       // format repo name
@@ -121,7 +71,8 @@ var getSportNews = function() {
       // create a link for each repo
       var repoEl = document.createElement("a");
       repoEl.classList = "list-item flex-row justify-space-between align-center";
-      repoEl.setAttribute("href", "./single-repo.html?repo=" + repoName);
+      //figure this part out
+      repoEl.setAttribute("href", "./countrynews.html?repo=" + repoName);
   
       // create a span element to hold repository name
       var titleEl = document.createElement("span");
@@ -153,6 +104,6 @@ var getSportNews = function() {
 
 
 
-  // add event listeners to forms
+// add event listeners to forms
 userFormEl.addEventListener("submit", formSubmitHandler);
 
