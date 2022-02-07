6d220f628edd555ea8f943515b7007980702b1f8
Jarod Florence <jarod.florence@outlook.com>
2022-02-06 18:56:24 -0600

    comments added and empty lines removed

--- assets/js/script.js
+++ assets/js/script.js
@@ -24,10 +24,10 @@ var formSubmitHandler = function(event) {
 // https://newsapi.org/v2/everything?q=tesla&from=2022-01-04&sortBy=publishedAt&apiKey=ade84378e67548e5b4ed1e45d4c09606
 // https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=ade84378e67548e5b4ed1e45d4c09606
 
-// fetch('https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=ade84378e67548e5b4ed1e45d4c09606')
-// fetch('https://api.sportsdata.io/v3/nfl/scores/json/News?key=95bd4e03de4e4fe0916f0c77516e239c')
-// .then(res=> res.json())
-// .then(data => console.log(data));
+// fetch('https://newsapi.org/v2/top-headlines?country=us&category=sports&apiKey=ade84378e67548e5b4ed1e45d4c09606')  <---- do not delete
+// fetch('https://api.sportsdata.io/v3/nfl/scores/json/News?key=95bd4e03de4e4fe0916f0c77516e239c')                   <---- do not delete
+// .then(res=> res.json())                                                                                           <---- do not delete
+// .then(data => console.log(data));                                                                                 <---- do not delete
 
 
 // first card
@@ -40,14 +40,12 @@ article1.innerText = data[0].Team;
 article1.id = 'card-team';
 article1.class = 'card-team';
 
-
 // create dynamic element and assign data from API to it
 let div = document.createElement('div');
 div.id = 'card-title';
 div.classname = 'card-title';
 document.getElementsByTagName('article')[0].appendChild(div);
 div.innerText = data[0].Title;
-
 
 // create dynamic element and assign data from API to it
 let innerDiv = document.createElement('div');
@@ -56,15 +54,19 @@ innerDiv.classname = 'card-content';
 div.appendChild(innerDiv);
 innerDiv.innerText = data[0].Content;
 
+// dynamic <br> element added
 let newInnerDiv = document.createElement('p');
 newInnerDiv.id = 'card-source';
 newInnerDiv.classname = 'card-source';
 innerDiv.appendChild(newInnerDiv);
 newInnerDiv.innerHTML = '<br>';
 
+// variables to allow link to be added from API fetch daat
 let linkText = 'Link to Source';
 let linkURL = data[0].OriginalSourceUrl
 
+//  dynamic button with link to source page
+// ---------------------------------------- would like to add a way to open in a new tab???
 let newerInnerDiv = document.createElement('button');
 newerInnerDiv.id = 'card-button';
 newerInnerDiv.classname = 'card-button';
@@ -82,14 +84,12 @@ article2.innerText = data[1].Team;
 article2.id = 'card-team';
 article2.class = 'card-team';
 
-
 // create dynamic element and assign data from API to it
 let div = document.createElement('div');
 div.id = 'card-title';
 div.classname = 'card-title';
 document.getElementsByTagName('article')[1].appendChild(div);
 div.innerText = data[1].Title;
-
 
 // create dynamic element and assign data from API to it
 let innerDiv = document.createElement('div');
@@ -98,15 +98,18 @@ innerDiv.classname = 'card-content';
 div.appendChild(innerDiv);
 innerDiv.innerText = data[1].Content;
 
+// dynamic <br> element added
 let newInnerDiv = document.createElement('p');
 newInnerDiv.id = 'card-source';
 newInnerDiv.classname = 'card-source';
 innerDiv.appendChild(newInnerDiv);
 newInnerDiv.innerHTML = '<br>';
 
+// vars to allow dynamic button to link
 let linkText = 'Link to Source';
 let linkURL = data[1].OriginalSourceUrl
 
+// dynamic button with link created
 let newerInnerDiv = document.createElement('button');
 newerInnerDiv.id = 'card-button';
 newerInnerDiv.classname = 'card-button';
@@ -124,14 +127,12 @@ article3.innerText = data[2].Team;
 article3.id = 'card-team';
 article3.class = 'card-team';
 
-
 // create dynamic element and assign data from API to it
 let div = document.createElement('div');
 div.id = 'card-title';
 div.classname = 'card-title';
 document.getElementsByTagName('article')[2].appendChild(div);
 div.innerText = data[2].Title;
-
 
 // create dynamic element and assign data from API to it
 let innerDiv = document.createElement('div');
@@ -140,15 +141,18 @@ innerDiv.classname = 'card-content';
 div.appendChild(innerDiv);
 innerDiv.innerText = data[2].Content;
 
+// dynamic <br> element added
 let newInnerDiv = document.createElement('p');
 newInnerDiv.id = 'card-source';
 newInnerDiv.classname = 'card-source';
 innerDiv.appendChild(newInnerDiv);
 newInnerDiv.innerHTML = '<br>';
 
+// vars to allow link on dynamic button
 let linkText = 'Link to Source';
 let linkURL = data[2].OriginalSourceUrl
 
+// dymanic button with link to source added
 let newerInnerDiv = document.createElement('button');
 newerInnerDiv.id = 'card-button';
 newerInnerDiv.classname = 'card-button';
